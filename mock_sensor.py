0c230013a9d6481baab5b620264adff0e8645714
chungyenyou <youchungyen@gmail.com>
2025-07-09 01:38:19 +0800

    feat: 模擬並處理感測器讀取失敗

--- mock_sensor.py
+++ mock_sensor.py
@@ -11,7 +11,12 @@ def read_mock_data():
     # 產生一個 40.0% 到 60.0% 之間的隨機濕度，並四捨五入到小數點後兩位
     mock_humidity = round(random.uniform(40.0, 60.0), 2)
 
-    return mock_temperature, mock_humidity
+    # 模擬感測器讀取失敗
+    chance = random.random()
+    if chance <= 0.1 :
+        return None, None
+    else:
+    	return mock_temperature, mock_humidity
 
 # --- 主程式從這裡開始 ---
 print("啟動模擬感測器... 每 2 秒讀取一次數據。")
@@ -20,13 +25,17 @@ print("按下 Ctrl+C 可以中斷程式。")
 try:
     while True:
         # 呼叫我們的模擬函數來取得數據
-        temp, humidity = read_mock_data()
+        temp, humidity  = read_mock_data()
 
-        # 使用 f-string 格式化輸出，讓版面更美觀
-        print(f"讀取成功: 溫度 = {temp}°C, 濕度 = {humidity}%")
+        if temp is None: 
+            print(f"讀取失敗，1秒後重試")
+            time.sleep(1)
 
-        # 等待 2 秒
-        time.sleep(2)
+        else:
+            # 使用 f-string 格式化輸出，讓版面更美觀
+            print(f"讀取成功: 溫度 = {temp}°C, 濕度 = {humidity}%")
+            # 等待 2 秒
+       	    time.sleep(2)
 
 except KeyboardInterrupt:
     print("\n程式已結束。")
